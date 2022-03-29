c6ef9bdce7067c3a66cd8e28bc2dddaf15ffd2d7
Oli Finn <oliver.james.finn@gmail.com>
2022-03-29 17:26:53 +0100

    partially completed, user can add photos but user journey needs to improve

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,9 +21,22 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:title, :summary, :description, :category, :area_id, :main_image)
+    params.require(:post).permit(:title, :summary, :description, :category, :area_id, :main_image, :images)
+  end
+
+  def post_params_images
+    params.require(:post).permit(:images)
   end
 end
