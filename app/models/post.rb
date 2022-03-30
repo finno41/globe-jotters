1b3e7a2991ebb602b995bee30243a3273a19d544
Oli Finn <oliver.james.finn@gmail.com>
2022-03-30 19:35:13 +0100

    validations added for posts and images hidden if not added

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   has_one_attached :main_image, dependent: :destroy
   has_many_attached :images, dependent: :destroy
   has_rich_text :description
+  validates :title, :summary, :description, :category, :main_image, presence: true
 end
